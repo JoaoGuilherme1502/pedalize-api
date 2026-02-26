02650ae2dc7fbed6d6d570da3c4e9a7f2e1bc23e
JoaoGuilherme1502 <jgsa1502@gmail.com>
2026-02-25 22:58:50 -0300

    feat: add get bike by id with 404

--- src/bike/bike.controller.ts
+++ src/bike/bike.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BikeService } from './bike.service';
 import { CreateBikeDto } from './dto/create-bike.dto';
 
@@ -14,5 +14,10 @@ export class BikeController {
     @Get()
     async findAll() {
         return this.bikeService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.bikeService.findOne(id);
     }
 }
--- src/bike/bike.service.ts
+++ src/bike/bike.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBikeDto } from './dto/create-bike.dto';
 import { BikeStatus } from '@prisma/client';
@@ -18,5 +18,17 @@ export class BikeService {
 
     async findAll() {
         return this.prisma.bike.findMany();
+    }
+
+    async findOne(id: string) {
+        const bike = await this.prisma.bike.findUnique({
+            where: { id },
+        });
+    
+        if (!bike) {
+            throw new NotFoundException('Bike não encontrada');
+        }
+
+        return bike;
     }
 }
